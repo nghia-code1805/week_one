b8eb77dd0e3a660a96b39fd5308062711276f8f2
Nghia <nghia.lahm12345@gmail.com>
2019-09-28 21:17:31 +0700

    week one

---
+++ khaibaosdbien.js
@@ -0,0 +1,27 @@
+var i=10;
+var f=20.5;
+var b=true;
+var s="hải phòng";
+var width =20;
+var height =10;
+var area = width*height;
+document.write("i = " +i);
+document.write("<br/>");
+document.write("f = " +f);
+document.write("<br/>");
+document.write("b = " +b);
+document.write("<br/>");
+document.write("s = " + s);
+document.write("<br/>");
+document.write("<br/>");
+document.write("area = " + area);
+document.write("<br/>");
+document.write("<br/>");
+a=prompt("nhập biến A: ");
+c=prompt("nhập biến B: ");
+if (a%c ==0){
+    alert('A chia hết cho B');
+} else {
+    alert('A không chia hết cho B');
+}
+
---
+++ toantu.js
@@ -0,0 +1,8 @@
+let inputwidth;
+let inputheight;
+inputwidth = prompt("nhập width");
+inputheight= prompt("nhập height");
+let width=parseInt(inputwidth);
+let height=parseInt(inputheight);
+let area = width*height;
+document.write("Diện Tích : " + area);
